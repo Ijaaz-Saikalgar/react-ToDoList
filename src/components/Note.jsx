405b3f4b6560181b218297653db13a6cd4e1db4a
ijaazz <ijaaz.designway@gmail.com>
2022-08-03 14:07:47 +0530

    todo checked updated.

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, {useState}from "react";
 
 function Note(props) {
+  const [style, setStyle] = useState(false);
+
   function handleClick() {
     props.onDelete(props.id);
   }
 
+  const changeStyle = () => {
+    setStyle(!style);
+  };
   return (
     <div className="note">
-      <ul><li>{props.content}</li></ul>
+      <ul><li onClick={changeStyle} className={style? "crossed-line":null}>{props.content}</li></ul>
       <button onClick={handleClick}>Delete</button>
     </div>
   );
